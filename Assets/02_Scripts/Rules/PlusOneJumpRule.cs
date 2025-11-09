ccb3d3ee67f8f80e99c6f7615a6c4b9daf5fc654
Stormsnap <Sebastian.Narjes@Games-Academy.de>
2025-11-09 02:07:45 +0100

    [+] added Rule and no slippery Material

---
+++ Assets/02_Scripts/Rules/PlusOneJumpRule.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PlusOneJumpRule : RuleFunction
+{
+    public override void Activate()
+    {
+        return;
+    }
+
+    public override void PlayerActivate(PlayerMovement player)
+    {
+        player.extraJumps++;
+    }
+
+    public override void WeaponActivate(Weapon weapon)
+    {
+        return;
+    }
+
+    public override void Deactivate()
+    {
+        return;
+    }
+
+    public override void PlayerDeactivate(PlayerMovement player)
+    {
+        player.extraJumps--;
+    }
+
+    public override void WeaponDeactivate(Weapon weapon)
+    {
+        return;
+    }
+}
